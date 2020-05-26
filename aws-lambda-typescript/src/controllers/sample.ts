e78916312b657c41815fe20e4927e12b0d9676da
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2020-05-26 00:20:50 -0300

    Update AWS lambda with TS boilerplate

--- aws-lambda-typescript/src/controllers/sample.ts
+++ aws-lambda-typescript/src/controllers/sample.ts
@@ -1,3 +1,5 @@
+import sampleRepo from "@src/ports/repos/sample";
+
 export interface CustomParams {
   readonly something: string;
 }
@@ -7,5 +9,10 @@ export interface SomeResult {
 }
 
 export default async (_params: CustomParams): Promise<SomeResult> => {
-  throw new Error("Function not implemented yet!");
+  // throw new Error("Function not implemented yet!");
+
+  console.log(await sampleRepo.getById(""));
+  return {
+    someOtherThing: "",
+  };
 };
--- aws-lambda-typescript/src/event-handlers/sample.ts
+++ aws-lambda-typescript/src/event-handlers/sample.ts
@@ -1,6 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import sampleController from "@src/controllers/sample";
 
-export default async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => ({
-  statusCode: 200,
-  body: JSON.stringify({}),
-});
+export default async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  await sampleController({ something: "" });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
+};
